0fe3b07832da042e0edbd7234425eec7b3b97180
VIPKarthikUK <98007681+VIPKarthikUK@users.noreply.github.com>
2022-08-19 19:02:58 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -19,10 +19,10 @@ class Config(object):
 	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
 	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
 	ABOUT_BOT_TEXT = f"""
-This is Permanent Files Store Bot!
-Send me any file I will save it in my Database. Also works for channel. Add me to channel as Admin with Edit Permission, I will add Save Uploaded File in Channel & add Sharable Button Link.
+**This is Permanent HMTD File Store Bot (4GB)!
+Send me any File I will save it in my Database. Also works for channel. Add me to channel as Admin with Edit Permission, I will add Save Uploaded File in Channel & add Sharable Button Link.**
 
-🤖 **My Name :** **[HMTD File Store (4GB)](https://t.me/{BOT_USERNAME})**
+🤖 **My Name :** **[HMTD File Store Bot (4GB)](https://t.me/{BOT_USERNAME})**
 
 📝 **Language :** **[Python3](https://www.python.org)**
 
@@ -43,13 +43,13 @@ Send me any file I will save it in my Database. Also works for channel. Add me t
 	ABOUT_DEV_TEXT = f"""
 🧑🏻‍💻 **Developer:** **@HMTD_Links**
 
-Developer is Super Noob. Just Learning from Official Docs. Please Donate the developer for Keeping the Service Alive.
+**Developer is Super Noob. Just Learning from Official Docs. Please Donate the developer for Keeping the Service Alive.**
 
-Also remember that developer will Delete Adult Contents from Database. So better don't Store Those Kind of Things.
+**Also remember that developer will Delete Adult Contents from Database. So better don't Store Those Kind of Things.**
 
 """
 	HOME_TEXT = """
-**Hi👋, [{}](tg://user?id={})\n\nThis is Permanent HMTD File Store (4GB).**
+**Hi👋, [{}](tg://user?id={})\n\nThis is Permanent HMTD File Store Bot (4GB).**
 
-**I'm an HMTD Official File Store (4GB) Maintained by @HMTD_Links.Send me any File I will Give you a Permanent Sharable Link. Keep me Join to Our Official Channel to Receive Bot & Movies Updates in @HMTD_Links. I have Support Channel Also! Check "About Bot" Button.**
+**I'm an HMTD Official File Store Bot (4GB) Maintained by @HMTD_Links.Send me any File I will Give you a Permanent Sharable Link. This Bot Support Upto 4GB File Also! Keep me Join to Our Official Channel to Receive Bot & Movies Updates in @HMTD_Links. I have Support Channel Also! Check "About Bot" Button.**
 """
